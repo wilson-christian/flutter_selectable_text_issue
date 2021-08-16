5ebf3e9f63defd32152ba760dce922ec03f2bc38
Wilson Christian <christian.wilson.ce@gmail.com>
2021-08-16 12:19:08 +0530

    Explained issue briefly for community to understand it easily

--- lib/main.dart
+++ lib/main.dart
@@ -50,36 +50,55 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          controller: _scrollController,
-          child: Container(
-            height: 170,
-            child: SelectableText.rich(
-              TextSpan(
-                children: [
-                  TextSpan(text: separatedText[0]),
-                  TextSpan(
-                    text: separatedText[1],
-                    style: TextStyle(color: Colors.pink),
-                  ),
-                  TextSpan(text: separatedText[2]),
-                ],
-              ),
-              onSelectionChanged: (selection, cause) {
-
-                // TRIED BELOW SOLUTION BUT IT FLICKERS IN SOME DEVICES
-
-                // Future.delayed(const Duration(milliseconds: 10), () {
-                //   _scrollController.jumpTo(_scrollController.offset);
-                // });
-              },
-              key: ValueKey("selectableText"),
-              // scrollPhysics: NeverScrollableScrollPhysics(),
-              toolbarOptions: ToolbarOptions(
-                  copy: false, cut: false, selectAll: false, paste: false),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Reproduce",
+              style: TextStyle(fontSize: 18),
             ),
-          ),
+            Text(
+              "1. Text is scrollable vertically \n2. Scroll to bottom & select any text \n3. When selecting initially it will auto scroll to top",
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            SingleChildScrollView(
+              controller: _scrollController,
+              child: Container(
+                height: 170,
+                child: SelectableText.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(text: separatedText[0]),
+                      TextSpan(
+                        text: separatedText[1],
+                        style: TextStyle(color: Colors.pink),
+                      ),
+                      TextSpan(text: separatedText[2]),
+                    ],
+                  ),
+                  onSelectionChanged: (selection, cause) {
+                    // TRIED BELOW SOLUTION BUT IT FLICKERS IN SOME DEVICES
+
+                    // Future.delayed(const Duration(milliseconds: 10), () {
+                    //   _scrollController.jumpTo(_scrollController.offset);
+                    // });
+                  },
+                  key: ValueKey("selectableText"),
+                  // scrollPhysics: NeverScrollableScrollPhysics(),
+                  toolbarOptions: ToolbarOptions(
+                      copy: false, cut: false, selectAll: false, paste: false),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
